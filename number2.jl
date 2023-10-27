0a14738da3d20bdf9a0cad2b85a34821efbb25c1
Luiza100000 <92116715+Luiza100000@users.noreply.github.com>
2023-10-27 13:33:07 +0300

    Update number2.jl

--- number2.jl
+++ number2.jl
@@ -1,34 +1,36 @@
 using HorizonSideRobots
-include("librobot.jl")
-function mark_frame_perimetr!(r::Robot) 
-
+function mark_frame_perimetr!(r::Robot)
     num_vert = moves!(r, Sud)
-
-    for side in sides
+    num_hor = moves!(r, West)
+    
+    for side in (Nord, Ost, Sud, West)
         putmarkers!(r, side)
-    end
+    end 
+    #УТВ: По всему периметру стоят маркеры
 
     moves!(r, Nord, num_vert)
+    moves!(r, Ost, num_hor)
+    #УТВ: Робот - в исходном положении
 end
 
-function moves!(r::Robot, side::HorizonSide)
-    n=0
-    while isborder(r, side) == false
-        move!(r, side)
-        n+=1
+function moves!(r::Robot,side::HorizonSide)
+    num_steps=0
+    while isborder(r,side)==false
+        move!(r,side)
+        num_steps+=1
     end
-    return n
+    return num_steps
 end
 
-function moves!(r::Robot, side::HorizonSide, n::Int)
-    for _ in 1:n
-        move!(r, side)
+function moves!(r::Robot,side::HorizonSide,num_steps::Int)
+    for _ in 1:num_steps # символ "_" заменяет фактически не используемую переменную
+        move!(r,side)
     end
 end
 
 function putmarkers!(r::Robot, side::HorizonSide)
-    while isborder(r, side)==false
-        move!(r, side)
+    while isborder(r,side)==false
+        move!(r,side)
         putmarker!(r)
     end
 end
