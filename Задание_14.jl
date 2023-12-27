61ba16369e3a182b823e2351c8ebac9857420bb3
Luiza100000 <92116715+Luiza100000@users.noreply.github.com>
2023-12-27 20:36:31 +0300

    Update Задание_14.jl

--- Задание_14.jl
+++ Задание_14.jl
@@ -12,27 +12,7 @@ include("Functions.jl")
 include("Struct.jl")
 
 inverse(s::HorizonSide) = HorizonSide(mod(Int(s)+2, 4))
-#=
-function try_move!(robot::ChessMarkRobot, side)
-    ortogonal_side = left(side)
-    back_side = inverse(ortogonal_side)
-    n=0
-    while isborder(robot, side)==true && isborder(robot, ortogonal_side) == false
-        move!(robot::ChessMarkRobot, ortogonal_side)
-        n += 1
-    end
-    if isborder(robot,side)==true
-        move!(robot::ChessMarkRobot, back_side, n)
-        return false
-    end
-    move!(robot, side)
-    if n > 0 # продолжается обход
-        along!(()->!isborder(robot, back_side), robot, side) 
-        move!(robot::ChessMarkRobot, back_side, n)
-    end
-    return true
-end
-=#
+
 function move(r, s)
     if !isborder(r,s)
         move!(r,s)
